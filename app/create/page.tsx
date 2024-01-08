1dc4833afd46189111afa8a47d23de5a11b72815
Shoma Nishitateno <shoma416@gmail.com>
2024-01-08 23:36:58 +0900

    on click test

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   CardTitle,
   CardDescription,
@@ -11,6 +13,10 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 export default function CreateRoom() {
+  const handleCreateRoom = () => {
+    alert("ルームを作成しました！");
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center  bg-gradient-to-r from-blue-500 to-blue-600 dark:from-blue-300 dark:to-blue-400">
       <Card className="w-full max-w-md">
@@ -21,15 +27,28 @@ export default function CreateRoom() {
         <CardContent className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="room-name">ルーム名</Label>
-            <Input id="room-name" required type="text" className="focus:border-blue-500 focus-visible:ring-0" />
+            <Input
+              id="room-name"
+              required
+              type="text"
+              className="focus:border-blue-500 focus-visible:ring-0"
+            />
           </div>
           <div className="space-y-2">
             <Label htmlFor="room-name">あなたの名前</Label>
-            <Input id="room-name" required type="text" className="focus:border-blue-500 focus-visible:ring-0" />
+            <Input
+              id="room-name"
+              required
+              type="text"
+              className="focus:border-blue-500 focus-visible:ring-0"
+            />
           </div>
         </CardContent>
         <CardFooter className="flex flex-col gap-2">
-          <Button className="w-full bg-blue-500 border-2 border-blue-500 hover:bg-white hover:text-blue-500 hover:border-blue-500">
+          <Button
+            className="w-full bg-blue-500 border-2 border-blue-500 hover:bg-white hover:text-blue-500 hover:border-blue-500"
+            onClick={handleCreateRoom}
+          >
             作成する
           </Button>
         </CardFooter>
