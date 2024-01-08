234aabb536fd5ba47bb8c2a6d3b362cecbeea990
Shoma Nishitateno <shoma416@gmail.com>
2024-01-08 23:28:37 +0900

    update design

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,35 +1,39 @@
-import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-export default function Component() {
+export default function CreateRoom() {
   return (
     <div className="min-h-screen flex items-center justify-center  bg-gradient-to-r from-blue-500 to-blue-600 dark:from-blue-300 dark:to-blue-400">
       <Card className="w-full max-w-md">
         <CardHeader>
-          <CardTitle className="text-2xl">Sign In</CardTitle>
-          <CardDescription>Please enter your email and password to login.</CardDescription>
+          <CardTitle className="text-2xl">ルームを作成する</CardTitle>
+          <CardDescription>新しくルームを作成します。</CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
           <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="m@example.com" required type="email" />
+            <Label htmlFor="room-name">ルーム名</Label>
+            <Input id="room-name" required type="text" className="focus:border-blue-500 focus-visible:ring-0" />
           </div>
           <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" required type="password" />
+            <Label htmlFor="room-name">あなたの名前</Label>
+            <Input id="room-name" required type="text" className="focus:border-blue-500 focus-visible:ring-0" />
           </div>
         </CardContent>
         <CardFooter className="flex flex-col gap-2">
-          <Button className="w-full">Sign in</Button>
-          <Link className="text-sm text-center" href="#">
-            Forgot Password?
-          </Link>
+          <Button className="w-full bg-blue-500 border-2 border-blue-500 hover:bg-white hover:text-blue-500 hover:border-blue-500">
+            作成する
+          </Button>
         </CardFooter>
       </Card>
     </div>
-  )
+  );
 }
-
