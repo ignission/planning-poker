dc26463db05d5b3e213af138c1b11c4bb4104bd6
Shoma Nishitateno <shoma416@gmail.com>
2024-05-05 22:48:37 +0900

    handle vote

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,29 @@ admin.initializeApp();
 export const handleVote = database.onValueWritten(
   "/rooms/{roomId}/users",
   (event) => {
-    logger.log("Event data: ", JSON.stringify(event.data));
-    logger.log("Event params: ", JSON.stringify(event.params));
+    const roomId = event.params.roomId;
+
+    logger.log("root id: " + roomId);
+
+    if (!event.data.after.exists()) {
+      logger.log("Data after is null");
+      return;
+    }
+
+    const original = event.data.after.val();
+    const list = Object.values(original);
+
+    if (list.length <= 1) {
+      logger.log("Not enough users to handle vote");
+      logger.log("List length: " + list.length);
+      return;
+    }
+
+    const voted = list.every((user: any) => user.point !== null && user.point !== undefined);
+    if (!voted) {
+      logger.log("Not all users have voted");
+      logger.log("List: " + JSON.stringify(list));
+      return;
+    }
   }
 );
