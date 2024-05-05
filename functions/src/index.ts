59477193f194a63ec4b913df31db9143bc4be89e
Shoma Nishitateno <shoma416@gmail.com>
2024-05-05 21:54:47 +0900

    init function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import admin = require("firebase-admin");
+import {onValueWritten} from "firebase-functions/lib/v2/providers/database";
+import {logger} from "firebase-functions";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.handleVote = onValueWritten(
+  "/rooms/${roomId}/users",
+  (event) => {
+    logger.log("Event data: ", event.data);
+  }
+);
